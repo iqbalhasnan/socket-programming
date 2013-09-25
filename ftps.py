76757ca2a14d89b86fcb002b77e0016a8250b10a
iqbalhasnan <iqbal.hasnan@i.edu.my>
2013-09-24 23:29:36 -0400

    refactor

--- ftps.py
+++ ftps.py
@@ -6,29 +6,26 @@ def main(port):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #bind port
     server.bind(('164.107.112.73', int(port)))
-    #set max accept rate to 10 conenctions
+    #set max accept rate to 5 conenctions
     server.listen(5)
     #server socket accept the client
     client, address = server.accept()
     #get 4-byte packet
-    packet = get_bytes(client, struct.calcsize('<I'))
+    size_bytes = client.recv(4)
     #unpack 4-byte packet that determine the size of image
-    size = struct.unpack('<I', packet)[0]
+    size = struct.unpack('<I', size_bytes)[0]
     #decode the 20-byte that determine the filename and strip the space
     filename =  client.recv(20).decode().lstrip()
-    #get the image data from client
-    image = get_bytes(client, size)
+    #get the remaining image bytes from client
+    get_bytes(client, size, filename)
     # Shutdown the socket and close the server conenction.
     server.shutdown(socket.SHUT_RDWR)
     server.close()
     #print the status
-    print('Server is receiving the data from client :)')
-    #write the image received to the current server directory
-    with open(filename, 'wb') as file:
-        file.write(image)
+    print('Server has received the data from the client! ')
 
-#method to return the number of bytes from client to the server
-def get_bytes(sock, size):
+#method to get the image bytes from client to the server
+def get_bytes(sock, size, filename):
     packet = bytearray()
     # Loop until all expected data is received.
     while len(packet) < size:
@@ -39,8 +36,9 @@ def get_bytes(sock, size):
             raise EOFError('Could not receive all expected data!')
         #append buffer into packet
         packet.extend(buffer)
-        #return the bytes packet
-    return bytes(packet)
+        #immediately write the bytes received to the file in current server directory
+        with open(filename, 'wb') as file:
+            file.write(packet)
 
 if __name__ == '__main__':
     try:
